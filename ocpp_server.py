007271a5186b4932798819120c5a0deeee118a59
avikhemka <avikhemka@icloud.com>
2023-06-28 17:10:47 +0545

    ocpp 2.0.1

--- ocpp_server.py
+++ ocpp_server.py
@@ -1,36 +1,54 @@
 import asyncio
-from datetime import datetime
-from ocpp.routing import on
-from ocpp.v20 import ChargePoint as cp
-from ocpp.v20 import call_result, call
-from websockets import serve
 import logging
+import websockets
+from datetime import datetime
 
-# Configure logging
+from ocpp.routing import on
+from ocpp.v201 import ChargePoint as cp
+from ocpp.v201 import call_result
+from ocpp.v201.enums import RegistrationStatusType
+
 logging.basicConfig(level=logging.INFO)
-logger = logging.getLogger("ocpp_server")
 
 class ChargePoint(cp):
-    @on(call.BootNotification)
-    def on_boot_notification(self, charging_station, reason, **kwargs):
-        print(f"Received BootNotification from charging station: {charging_station}")
+    @on('BootNotification')
+    async def on_boot_notification(self, charging_station, reason, **kwargs):
         return call_result.BootNotificationPayload(
-            current_time=datetime.utcnow().strftime('%Y-%m-%dT%H:%M:%SZ'),
+            current_time=datetime.utcnow().isoformat(),
             interval=10,
-            status="Accepted"
+            status=RegistrationStatusType.accepted
         )
 
 async def on_connect(websocket, path):
     try:
-        charge_point = ChargePoint("my_charge_point", websocket)
-        await charge_point.start()
-    except Exception as e:
-        logger.exception(f"Error occurred: {e}")
+        requested_protocols = websocket.request_headers['Sec-WebSocket-Protocol']
+    except KeyError:
+        logging.info("Client hasn't requested any Subprotocol. Closing Connection")
+        return await websocket.close()
 
-try:
-    server = serve(on_connect, "0.0.0.0", 9000)
-    print("OCPP server started. Awaiting connections...")
-    asyncio.get_event_loop().run_until_complete(server)
-    asyncio.get_event_loop().run_forever()
-except Exception as e:
-    print(f"Failed to start OCPP server: {e}")
+    if websocket.subprotocol:
+        logging.info("Protocols Matched: %s", websocket.subprotocol)
+    else:
+        logging.warning('Protocols Mismatched | Expected Subprotocols: %s,'
+                        ' but client supports  %s | Closing connection',
+                        websocket.available_subprotocols,
+                        requested_protocols)
+        return await websocket.close()
+
+    charge_point_id = path.strip('/')
+    cp_instance = ChargePoint(charge_point_id, websocket)
+
+    await cp_instance.start()
+
+async def main():
+    server = await websockets.serve(
+        on_connect,
+        '0.0.0.0',
+        9000,
+        subprotocols=['ocpp2.0.1']
+    )
+    logging.info("WebSocket Server Started")
+    await server.wait_closed()
+
+if __name__ == '__main__':
+    asyncio.run(main())
--- simulate_charger.py
+++ simulate_charger.py
@@ -1,35 +1,22 @@
 import asyncio
-import json
 import websockets
+from ocpp.v201 import call
 
 async def simulate_charger():
-    try:
-        async with websockets.connect('ws://192.168.1.131:9000/') as websocket:
-            try:
-                # Send a BootNotification message
-                boot_notification_request = {
-                    "messageTypeId": 2,
-                    "uniqueId": "1234",
-                    "payload": {
-                        "chargePointModel": "TestCharger",
-                        "chargePointVendor": "ACME",
-                        "chargePointSerialNumber": "123456789",
-                        "chargeBoxSerialNumber": "123456789",
-                        "firmwareVersion": "1.0",
-                    }
-                }
+    async with websockets.connect('ws://192.168.1.119:9000/', subprotocols=['ocpp2.0.1']) as websocket:
+        try:
+            request = call.BootNotificationPayload(
+                charging_station={
+                    'model': 'Wallbox XYZ',
+                    'vendor_name': 'anewone'
+                },
+                reason="PowerUp"
+            )
+            await websocket.send(request.to_json())
+            response = await websocket.recv()
+            print(f"Received response: {response}")
 
-                await websocket.send(json.dumps(boot_notification_request))
-                print("Sent BootNotification request.")
-            except Exception as e:
-                print(f"Failed to send BootNotification request: {e}")
-
-            try:
-                response = await websocket.recv()
-                print(f"Received response: {response}")
-            except Exception as e:
-                print(f"Failed to receive response: {e}")
-    except Exception as e:
-        print(f"Failed to connect to the server: {e}")
+        except Exception as e:
+            print(f"Error occurred: {e}")
 
 asyncio.run(simulate_charger())
