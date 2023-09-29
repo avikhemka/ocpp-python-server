0aac99ed7bee454f9a2c38fca705359bb68974c0
avikhemka <avikhemka@icloud.com>
2023-09-30 05:33:13 +0800

    changed from ocpp201 to ocpp16

--- ocpp_server.py
+++ ocpp_server.py
@@ -2,56 +2,75 @@ import asyncio
 import logging
 from datetime import datetime
 
-from ocpp.routing import on
-from ocpp.v201 import ChargePoint as cp
-from ocpp.v201 import call_result
-from ocpp.v201.enums import RegistrationStatusType
-from ocpp.v201 import call
+try:
+    import websockets
+except ModuleNotFoundError:
+    print("This example relies on the 'websockets' package.")
+    print("Please install it by running: ")
+    print()
+    print(" $ pip install websockets")
+    import sys
 
-import websockets
+    sys.exit(1)
+
+from ocpp.routing import on
+from ocpp.v16 import ChargePoint as cp
+from ocpp.v16 import call_result
+from ocpp.v16.enums import Action, RegistrationStatus
 
 logging.basicConfig(level=logging.INFO)
 
-class ChargePoint(cp):
-    async def send_boot_notification(self, conf):
-        await self.send_call_result('BootNotification', conf)
 
-    @on('BootNotification')
-    def on_boot_notification(self, charging_station, reason, **kwargs):
-        print(f"Received boot notification from {charging_station['vendor_name']}, model {charging_station['model']}")
+class ChargePoint(cp):
+    @on(Action.BootNotification)
+    def on_boot_notification(
+        self, charge_point_vendor: str, charge_point_model: str, **kwargs
+    ):
         return call_result.BootNotificationPayload(
             current_time=datetime.utcnow().isoformat(),
             interval=10,
-            status=RegistrationStatusType.accepted,
+            status=RegistrationStatus.accepted,
         )
 
-    @on(call.AuthorizePayload)
-    def on_authorize(self, id_tag, **kwargs):
-        print(f"Received authorize request for tag: {id_tag}")
-
-        # Implement your authorization logic here
-
-        return call_result.AuthorizePayload(
-            status="Accepted"  # Or "Rejected" based on your authorization logic
-        )
-
-    @on(call.HeartbeatPayload)
-    def on_heartbeat(self, **kwargs):
-        print("Received heartbeat request")
-
-        return call_result.HeartbeatPayload(
-            current_time=datetime.utcnow().isoformat()
-        )
 
 async def on_connect(websocket, path):
-    logging.info('WebSocket Server Started')
-    charge_point_id = path.strip('/')
+    """For every new charge point that connects, create a ChargePoint
+    instance and start listening for messages.
+    """
+    try:
+        requested_protocols = websocket.request_headers["Sec-WebSocket-Protocol"]
+    except KeyError:
+        logging.error("Client hasn't requested any Subprotocol. Closing Connection")
+        return await websocket.close()
+    if websocket.subprotocol:
+        logging.info("Protocols Matched: %s", websocket.subprotocol)
+    else:
+        # In the websockets lib if no subprotocols are supported by the
+        # client and the server, it proceeds without a subprotocol,
+        # so we have to manually close the connection.
+        logging.warning(
+            "Protocols Mismatched | Expected Subprotocols: %s,"
+            " but client supports  %s | Closing connection",
+            websocket.available_subprotocols,
+            requested_protocols,
+        )
+        return await websocket.close()
+
+    charge_point_id = path.strip("/")
     cp = ChargePoint(charge_point_id, websocket)
 
     await cp.start()
 
 
-server = websockets.serve(on_connect, '0.0.0.0', 9000, subprotocols=['ocpp2.0.1'])
+async def main():
+    server = await websockets.serve(
+        on_connect, "0.0.0.0", 9000, subprotocols=["ocpp1.6"]
+    )
 
-asyncio.get_event_loop().run_until_complete(server)
-asyncio.get_event_loop().run_forever()
+    logging.info("Server Started listening to new connections...")
+    await server.wait_closed()
+
+
+if __name__ == "__main__":
+    # asyncio.run() is used when running this example with Python >= 3.7v
+    asyncio.run(main())
--- simulate_charger.py
+++ simulate_charger.py
@@ -1,55 +1,47 @@
 import asyncio
 import logging
-from ocpp.v201 import ChargePoint as cp
-import websockets
-import ocpp.v201.call as call
 
-print(help(call.AuthorizePayload))
+try:
+    import websockets
+except ModuleNotFoundError:
+    print("This example relies on the 'websockets' package.")
+    print("Please install it by running: ")
+    print()
+    print(" $ pip install websockets")
+    import sys
+
+    sys.exit(1)
+
+
+from ocpp.v16 import ChargePoint as cp
+from ocpp.v16 import call
+from ocpp.v16.enums import RegistrationStatus
+
 logging.basicConfig(level=logging.INFO)
 
-class Charger(cp):
+
+class ChargePoint(cp):
     async def send_boot_notification(self):
         request = call.BootNotificationPayload(
-            charging_station={
-                "model": "Wallbox XYZ",
-                "vendor_name": "anewone",
-            },
-            reason="PowerUp",
+            charge_point_model="Optimus", charge_point_vendor="The Mobility House"
         )
+
         response = await self.call(request)
 
-        if response.status == "Accepted":
+        if response.status == RegistrationStatus.accepted:
             print("Connected to central system.")
 
-    async def send_authorize(self, id_tag):
-        token = {"id_token_type": "ISO14443", "id_token": id_tag}
-        request = call.AuthorizePayload(id_token=token)
-        response = await self.call(request)
-
-        if response.status == "Accepted":
-            print(f"Authorization accepted for tag: {id_tag}")
-        else:
-            print(f"Authorization denied for tag: {id_tag}")
-
-    async def send_heartbeat(self):
-        request = call.HeartbeatPayload()
-        response = await self.call(request)
-
-        print(f"Received heartbeat response with current time: {response.current_time}")
 
 async def main():
     async with websockets.connect(
-        'ws://175.41.149.142:9000/CP_1',  # Changed to AWS IP
-        subprotocols=['ocpp2.0.1']
+        "ws://175.41.149.142:9000/CP_1", subprotocols=["ocpp1.6"]
     ) as ws:
-        charger = Charger('CP_1', ws)
 
-        await asyncio.gather(
-            charger.start(),
-            charger.send_boot_notification(),
-            charger.send_authorize("123456"),
-            charger.send_heartbeat(),
-        )
+        cp = ChargePoint("CP_1", ws)
 
-if __name__ == '__main__':
+        await asyncio.gather(cp.start(), cp.send_boot_notification())
+
+
+if __name__ == "__main__":
+    # asyncio.run() is used when running this example with Python >= 3.7v
     asyncio.run(main())
